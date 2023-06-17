67a2358487fbcb015d3255e9abfe98cd88171fb9
Marc Ivan Guillermo <61181505+aerahrah@users.noreply.github.com>
2023-06-17 20:04:21 +0800

    Add signin with guest account

--- client/src/components/auth/signup.jsx
+++ client/src/components/auth/signup.jsx
@@ -1,18 +1,33 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useNavigate  } from "react-router-dom";
 import Timer from "../utils/messageTimeout";
+import { setCookie, getCookie } from "../utils/cookieUtils";
+
 const Signup = () =>{
+    const navigate = useNavigate();
     const url ="http://localhost:3500/";
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [message, setMessage] = useState('')
-
+    const guestUsername = "WiMt6FagfShTkTobAs76x9zCwgQ7MOid"
+    const guestPassword = "hdUCnPLZ4oBGPhFy7Wz6DalGpJZhhJhL"
     const handleSignup = async() =>{
         try {
-            const response = await Axios.post(`${url}auth/signup`,  {username: username, password: password})
+            const response = await Axios.post(`${url}auth/signup`,  {username: username, password: guestPassword})
             const {message} = response.data;
             setMessage(message)
+        } catch (error) {
+            setMessage(error.response.data.message)
+        }
+    }
+    const handleGuestSignin = async() =>{
+        try {
+            const response = await Axios.post(`${url}auth/signin`,{username:guestUsername,password: guestPassword})
+            const {message, token} = response.data;
+            setMessage(message);
+            setCookie("token", token, 1)
+            navigate("/quote-generator");
         } catch (error) {
             setMessage(error.response.data.message)
         }
@@ -34,6 +49,7 @@ const Signup = () =>{
                 onChange={(e) => setPassword(e.target.value)}
                 />
                 <button onClick={handleSignup}>Sign up</button>
+                <button onClick={handleGuestSignin}>Signin with guest account</button>
                 <Link to = "/signin">sign in</Link>
             </div>
             {message && (
